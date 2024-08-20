50a978acbb93b5f26b6d71fe0758721b96c3301d
gxct <110650911+gxct@users.noreply.github.com>
2024-08-20 08:47:59 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -41,7 +41,7 @@ $db->addServer(array (
   'password' => '数据库密码',
   'charset' => 'utf8mb4',
   'port' => '3306',
-  'database' => '数据库昵称',
+  'database' => '数据库名称',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
